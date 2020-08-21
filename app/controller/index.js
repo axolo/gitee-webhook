579d10c2e70f2f565b2f189db108b29e88c3a679
Yueming Fang <zergdo@gmail.com>
2020-08-21 16:13:59 +0800

    fix: change refs to ref

--- app/controller/index.js
+++ app/controller/index.js
@@ -2,28 +2,34 @@
 
 const Controller = require('egg').Controller;
 
-class HomeController extends Controller {
+class IndexController extends Controller {
   async create() {
     const { ctx, app, service } = this;
     const { body } = ctx.request;
-    const { repository: { url } = {}, hook_name, timestamp, sign, refs } = body;
+    const { repository: { url } = {}, hook_name, timestamp, sign, ref } = body;
     // 匹配勾子
     const hook = app.config.hooks.find(h => h.repository.url === url && h.hook_name === hook_name);
     if (!hook) {
-      ctx.body = { errcode: 404, errmsg: 'hooks not found', errinfo: { url, hook_name } };
+      const errcode = 404;
+      ctx.status = errcode;
+      ctx.body = { errcode, errmsg: 'hook invalid', errinfo: { url, hook_name } };
       return;
     }
     // 匹配分支
     const { secret, exec } = hook;
-    if (hook.refs && hook.refs !== refs) {
-      ctx.body = { errcode: 403, errmsg: 'branch not match', errinfo: refs };
+    if (hook.ref && hook.ref !== ref) {
+      const errcode = 403;
+      ctx.status = errcode;
+      ctx.body = { errcode, errmsg: 'ref invalid', errinfo: ref };
       return;
     }
     // 校验签名
     const text = timestamp + '\n' + secret;
     const hmac = service.webhook.sign(secret, text);
     if (hmac !== sign) {
-      ctx.body = { errcode: 401, errmsg: 'sign not match', errinfo: { sign } };
+      const errcode = 401;
+      ctx.status = errcode;
+      ctx.body = { errcode: 401, errmsg: 'sign invalid', errinfo: { sign } };
       return;
     }
     // 匹配及校验成功
@@ -39,4 +45,4 @@ class HomeController extends Controller {
   }
 }
 
-module.exports = HomeController;
+module.exports = IndexController;
