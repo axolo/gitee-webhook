d402ebad1f7589ba6ca452693a9b1e250fc94b39
Yueming Fang <zergdo@gmail.com>
2020-08-26 16:01:45 +0800

    fix: service.webhook.exec

--- app/service/webhook.js
+++ app/service/webhook.js
@@ -6,13 +6,13 @@ const exec = util.promisify(require('child_process').exec);
 const Service = require('egg').Service;
 
 class WebhookService extends Service {
+  get exec() {
+    return exec;
+  }
+
   sign(secret, text) {
     const hmac = crypto.createHmac('sha256', secret).update(text).digest('base64');
     return hmac;
-  }
-
-  exec(...args) {
-    return exec(...args);
   }
 }
 
